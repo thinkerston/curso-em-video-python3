85734a9ac0cc8a2b1ab69e8330382eea15824239
Julio Santos <thinkerston97@gmail.com>
2019-12-19 13:20:20 -0400

    dissecando uma variável

---
+++ mundo-01/exercicio-004.py
@@ -0,0 +1,7 @@
+''' Crie um programa que leia algo pelo teclado e mostre o seu tipo primitivo e todas as informações possiveis sobre ela'''
+
+
+reader = input('Digite algo: ')
+print(type(reader))
+ 
+
