6fe95b4fc6015462052259d28091113cbc6ed1b1
Julio Santos <thinkerston97@gmail.com>
2019-12-19 12:59:52 -0400

    corrigindo bugs

--- mundo-01/exercicio-003.py
+++ mundo-01/exercicio-003.py
@@ -4,4 +4,5 @@ print('insira dois numeros para fazer a soma')
 primeiroNumero = float(input('Digite o primeiro numero: '))
 segundoNumero = float(input('Digite o segundo numero: '))
 soma = float(primeiroNumero + segundoNumero)
-print(f'a soma dos numeros é: {soma}')
+print(f'a soma entre {primeiroNumero} e {segundoNumero} vale: {soma}')
+
