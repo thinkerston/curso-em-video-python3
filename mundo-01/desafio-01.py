d58bb20afca1b81da0b9e80c105c9dce2f957e64
Julio Santos <thinkerston97@gmail.com>
2019-12-19 09:59:44 -0400

    deleted files

--- mundo-01/desafio-01.py
+++
@@ -1,5 +0,0 @@
-'''Crie um script Python que leia o nome dessa pessoa e mostre uma mensagem de boas-vindas de acordo com o valor digitado'''
-
-
-name = str(input('What your name? '))
-print(f'hello {name}! nice to meet you!')
--- mundo-01/desafio-02.py
+++
@@ -1,9 +0,0 @@
-''' Crie um script Python que leia o dia o mês e o ano de nascimento de uma pessoa e mostre uma mensagem com a data formatada'''
-
-
-day = str(input('what day you were born? '))
-month = str(input('and the month? '))
-year = str(input('what year? '))
-
-
-print(f'your born in day {day} of the {month} in year {year}') 
