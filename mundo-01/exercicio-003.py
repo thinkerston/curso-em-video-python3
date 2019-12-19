39d35effc5f37ff1571cad620248435101afed08
Julio Santos <thinkerston97@gmail.com>
2019-12-19 13:06:07 -0400

    programa que soma dois numeros

--- mundo-01/exercicio-003.py
+++ mundo-01/exercicio-003.py
@@ -1,5 +1,6 @@
 '''crie um programa que peça a usuário dois numeros e faça a soma'''
 
+
 print('insira dois numeros para fazer a soma')
 primeiroNumero = float(input('Digite o primeiro numero: '))
 segundoNumero = float(input('Digite o segundo numero: '))
