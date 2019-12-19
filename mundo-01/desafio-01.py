3cd4ef163444ed7a2eba3cb97fb0fefe1af5f202
Julio Santos <thinkerston97@gmail.com>
2019-12-19 09:14:29 -0400

    create three files and resolution exercices

---
+++ mundo-01/desafio-01.py
@@ -0,0 +1,5 @@
+'''Crie um script Python que leia o nome dessa pessoa e mostre uma mensagem de boas-vindas de acordo com o valor digitado'''
+
+
+name = str(input('What your name? '))
+print(f'hello {name}! nice to meet you!')
---
+++ mundo-01/desafio-03.py
@@ -0,0 +1,11 @@
+'''Crie um script python que leia dos numeros e tente mostrar a soma entre eles'''
+
+
+print('digite dois numeros para que eu possa fazer a soma')
+firstNumber = float(input('digite um numero: '))
+secondNumber = float(input('digite o segundo numero: '))
+sumBoth = (firstNumber + secondNumber)
+
+
+print(f'A soma entre {firstNumber} e {secondNumber} é {sumBoth}')
+
