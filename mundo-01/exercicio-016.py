505c50bf8cd03f0f3b09030a6f3dc11d881a0b89
Julio Santos <thinkerston97@gmail.com>
2019-12-20 17:58:09 -0400

    truncando o numero

---
+++ mundo-01/exercicio-016.py
@@ -0,0 +1,7 @@
+from math import trunc
+
+'''crie uma função que leia um numero real qualquer pelo teclado e mostre na tela a sua porção inteira'''
+
+numero = float(input('insira um numero real: '))
+print(f'a parte inteira é {trunc(numero)}')
+
