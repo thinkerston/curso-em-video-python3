83c1efbc3527ee2452f9df521f7afd855505dea7
Julio Santos <thinkerston97@gmail.com>
2019-12-20 10:29:41 -0400

    Calculando a média de duas notas

---
+++ mundo-01/exercicio-007.py
@@ -0,0 +1,7 @@
+'''desenvolva um program que leia as duas notas de um aluno, calcule e moste  a sua média'''
+
+primeiraNota = float(input('Digite a primeira nota: '))
+segundaNota = float(input('Digite a segunda nota: '))
+media = float((primeiraNota + segundaNota) / 2)
+
+print(f'a media do aluno foi {media}')
