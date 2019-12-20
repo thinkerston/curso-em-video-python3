7782bf5739ffff227e28db881a3ae5b7a7dd4878
Julio Santos <thinkerston97@gmail.com>
2019-12-20 10:34:25 -0400

    convertendo metros em centimetros e milimetros

---
+++ mundo-01/exercicio-008.py
@@ -0,0 +1,4 @@
+'''Crie um programa que leia um valor em metros e o exiba convertido em centimetros e milimetros'''
+
+metros = float(input('Insira um numero para converter em centimetros e milimetros: '))
+print(f'{metros} em centimetros é {metros * 100} e em milimetros é {metros * 1000}')
