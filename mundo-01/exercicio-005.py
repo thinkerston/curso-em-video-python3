39b9a210aa66a4df21b504088c7b75aa411575a8
Julio Santos <thinkerston97@gmail.com>
2019-12-20 10:17:47 -0400

    Predecessor e Antecessor

---
+++ mundo-01/exercicio-005.py
@@ -0,0 +1,6 @@
+''' Faça um programa que leia um numero inteiro e mostre
+o seu sucessor e antecessor'''
+
+print('digite um numero para ver o seu antecessor e sucessor')
+numero = int(input('Digite o numero: '))
+print(f'o antecessor de {numero} é {numero -1} e o seu sucessor é {numero +1}')
