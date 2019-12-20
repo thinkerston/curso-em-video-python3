d7078168c56c57a693285d0d8386dbf21b61a7ac
Julio Santos <thinkerston97@gmail.com>
2019-12-20 19:38:21 -0400

    criando um programa que escolhe entre 4 alunos

--- mundo-01/exercicio-019.py
+++ mundo-01/exercicio-019.py
@@ -6,8 +6,8 @@ nome do escolhido na tela'''
 
 aluno1 = str(input('Insira o nome do primeiro aluno: '))
 aluno2 = str(input('Insira o nome do segundo aluno: '))
-aluno3 = str(input('Insira o nome do segundo aluno: '))
-aluno4 = str(input('Insira o nome do segundo aluno: '))
+aluno3 = str(input('Insira o nome do terceiro aluno: '))
+aluno4 = str(input('Insira o nome do quarto aluno: '))
 
 print('O aluno escolhido é ', end='')
 print(choice([aluno1, aluno2, aluno3, aluno4]))
