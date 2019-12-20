b2300798eb1761ff566b3afe1d9a990116eab6e7
Julio Santos <thinkerston97@gmail.com>
2019-12-20 10:38:53 -0400

    calculando a tabuada

---
+++ mundo-01/exercicio-009.py
@@ -0,0 +1,13 @@
+'''Faca um programa que leia um numero inteiro qualquer e mostre na tela a sua tabuada até 10'''
+
+numero = int(input('insira um numero para ver a tabuada até 10: '))
+print(f'{numero} x 1 = {numero * 1}')
+print(f'{numero} x 2 = {numero * 2}')
+print(f'{numero} x 3 = {numero * 3}')
+print(f'{numero} x 4 = {numero * 4}')
+print(f'{numero} x 5 = {numero * 5}')
+print(f'{numero} x 6 = {numero * 6}')
+print(f'{numero} x 7 = {numero * 7}')
+print(f'{numero} x 8 = {numero * 8}')
+print(f'{numero} x 9 = {numero * 9}')
+print(f'{numero} x 10 = {numero * 10}')
