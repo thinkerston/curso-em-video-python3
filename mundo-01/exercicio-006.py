db860967419ec03b2cf2d3533784a3209de08a55
Julio Santos <thinkerston97@gmail.com>
2019-12-20 10:23:53 -0400

    calculando dobro, triplo e raiz

---
+++ mundo-01/exercicio-006.py
@@ -0,0 +1,4 @@
+'crie um algoritmo que leia um numero e moste seu dobro seu triplo e raiz quadrada'
+
+numero = float(input('digite um numero para ver o dobro, triplo e raiz quadrada: '))
+print(f'o dobro de {numero} é {numero * 2} o triplo é {numero * 3} e a sua raiz é {numero ** (1/2)}')
