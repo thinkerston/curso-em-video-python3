362eb35f89ae0b5af6c295f1d6979111f15d631e
Julio Santos <thinkerston97@gmail.com>
2019-12-21 17:49:01 -0400

    criando um triangulo

---
+++ mundo-01/exercicio-035.py
@@ -0,0 +1,13 @@
+'''Desenvolva um programa que leia o comprimento de três retas e diga
+ao usuário se elas podem ou não formar um triângulo'''
+
+primeiraReta = float(input('Insira a Primeira reta: '))
+segundaReta = float(input('Insira a Segunda reta: '))
+terceiraReta = float(input('Insira a Terceira reta: '))
+
+if (abs(segundaReta - terceiraReta) < primeiraReta) and (primeiraReta < (segundaReta + terceiraReta)):
+    if (abs(primeiraReta - terceiraReta) < segundaReta) and (segundaReta < ( primeiraReta + terceiraReta)):
+        if (abs(primeiraReta - segundaReta) < terceiraReta) and (terceiraReta < (primeiraReta + segundaReta)):
+            print('Os seus dados formam um triângulo')
+else
+    print('os seus dados não sao capazes de formar um triângulo')
