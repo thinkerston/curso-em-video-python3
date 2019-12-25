74fb1925322c36a4a2decbb5f4e1fbb9eb08c86f
Julio Santos <thinkerston97@gmail.com>
2019-12-25 10:23:01 -0400

    Estudando a Estrutura de repetição "while"

--- mundo-02/exercicio-064.py
+++ mundo-02/exercicio-064.py
@@ -3,17 +3,18 @@ o programa só vai parar quando usuário digitar valor 999,
 que é a condição de parada. no final mostre quantos numeros foram digitados
 e qual foi a soma entre eles (desconsiderando a flag)'''
 
-numeros = int(input('insira numeros para somar e 999 para sair: '))
+
 sair = bool(False)
 somador = int(0)
 numeroDeVezes = int(0)
 
 while sair == False:
+    numeros = int(input('insira numeros para somar e 999 para sair: '))
     if numeros == 999:
         sair = bool (True)
         pass
     else:
-        numeros = int(input('insira numeros para somar e 999 para sair: '))
+        
         if numeros != 999:
             numeroDeVezes += 1
             somador += numeros
