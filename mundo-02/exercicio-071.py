ab11d4f531ad60f3b88df0a919c33eaf0fda9c69
Julio Santos <thinkerston97@gmail.com>
2019-12-26 12:45:58 -0400

    Interrompendo repetições While com break

--- mundo-02/exercicio-071.py
+++ mundo-02/exercicio-071.py
@@ -3,29 +3,40 @@ pergunte ao usuário qual será o valor sacado (numero inteiro) e o programa vai
 quantas cedulas de cada valor serão entregues
 obs.: considere que o caixa possui cédulas  de R$50, R$20, R$10 e R$1.'''
 
-print('O caixa possui cedulas de R$50, R$20, R$10 e R$1')
-valorSacado = int(input('Insira o valor a ser sacado: '))
-valorRestante = int(0)
-notasCinquenta = int(0)
-notasVinte = int(0)
-notasDez = int(0)
-notasUm = int(0)
 
-notasCinquenta = valorSacado // 50
-valorRestante = valorSacado % 50
 
-notasVinte = valorRestante // 20
-valorRestante = valorRestante % 20
+while True:
+    print('O caixa possui cedulas de R$50, R$20, R$10 e R$1')
+    valorSacado = int(input('Insira o valor a ser sacado: '))
+    valorRestante = int(0)
+    notasCinquenta = int(0)
+    notasVinte = int(0)
+    notasDez = int(0)
+    notasUm = int(0)
+        
+    notasCinquenta = valorSacado // 50
+    valorRestante = valorSacado % 50
 
-notasDez = valorRestante // 10
-valorRestante = valorRestante % 10
+    notasVinte = valorRestante // 20
+    valorRestante = valorRestante % 20
 
-notasUm = valorRestante // 1
-valorRestante = valorRestante % 1
+    notasDez = valorRestante // 10
+    valorRestante = valorRestante % 10
 
-print(notasCinquenta)
-print(notasVinte)
-print(notasDez)
-print(notasUm)
+    notasUm = valorRestante // 1
+    valorRestante = valorRestante % 1
+    
+    print(f'Total de {notasCinquenta} Notas de R$ 50')
+    print(f'Total de {notasVinte} Notas de R$ 20')
+    print(f'Total de {notasDez} Notas de R$ 10')
+    print(f'Total de {notasUm} Notas de R$ 1')
 
-'''Optei fazer sem While por ser mais facil'''
+
+    novaOperacao = str(input('Deseja fazer uma nova operacao? [Y/N] ')).lower()
+    while novaOperacao not in 'yn':
+        novaOperacao = str(input('Deseja fazer uma nova operacao? [Y/N] ')).lower()
+    
+    if novaOperacao == 'n':
+        print('Hasta la vista baby')
+        break
+    print('========' * 10)
